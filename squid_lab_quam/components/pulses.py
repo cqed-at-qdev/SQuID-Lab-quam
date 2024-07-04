9e35eea9928f9b2043474754a1cf0b3cdd5e98de
JacobHast <jacobhastrup@gmail.com>
2024-07-04 09:03:05 +0200

    Added FlatTopCosinePulse

--- squid_lab_quam/components/pulses.py
+++ squid_lab_quam/components/pulses.py
@@ -1,4 +1,7 @@
-from quam.components.pulses import DragPulse
+from typing import Literal
+
+import numpy as np
+from quam.components.pulses import DragPulse, Pulse
 from quam.core import quam_dataclass
 
 __all__ = ["DragGaussianPulse"]
@@ -9,3 +12,45 @@ class DragGaussianPulse(DragPulse):
     """Rename of DragPulse to DragGaussianPulse."""  # TODO: also rename alpha to drag_coefficient
 
     pass
+
+
+@quam_dataclass
+class FlatTopCosinePulse(Pulse):
+    """Gaussian pulse with flat top QuAM component.
+
+    Args:
+        length (int): The total length of the pulse in samples.
+        amplitude (float): The amplitude of the pulse in volts.
+        axis_angle (float, optional): IQ axis angle of the output pulse in radians.
+            If None (default), the pulse is meant for a single channel or the I port
+                of an IQ channel
+            If not None, the pulse is meant for an IQ channel (0 is X, pi/2 is Y).
+        flat_length (int): The length of the pulse's flat top in samples.
+            The rise and fall lengths are calculated from the total length and the
+            flat length.
+    """
+
+    amplitude: float
+    axis_angle: float = None
+    flat_length: int = 0
+    return_part: Literal["all", "flat", "rise"] = "all"
+
+    @property
+    def rise_fall_length(self):
+        return (self.length - self.flat_length) // 2
+
+    def waveform_function(self):
+        from qualang_tools.config.waveform_tools import flattop_cosine_waveform
+
+        waveform = flattop_cosine_waveform(
+            amplitude=self.amplitude,
+            flat_length=self.flat_length,
+            rise_fall_length=self.rise_fall_length,
+            return_part=self.return_part,
+        )
+        waveform = np.array(waveform)
+
+        if self.axis_angle is not None:
+            waveform = waveform * np.exp(1j * self.axis_angle)
+
+        return waveform
